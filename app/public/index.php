ef97e6047cb939ee15038b37c34e35dc079e42a6
ddc <2223445830@qq.com>
2019-01-23 23:47:55 +0800

    php

--- app/public/index.php
+++ app/public/index.php
@@ -1,7 +1,16 @@
 <?php
 
-phpinfo();
+error_reporting(2048);
 
+define("APP_PATH", dirname(__DIR__));
 
+$log = APP_PATH."/runtime/1.txt";
+echo $log;
+
+var_dump(file_put_contents( $log, "hello"));
+
+echo 1;
+
+d();
 
 
